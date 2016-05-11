5736cb47fc475bdbfabb4f967463ac71573c2464
JunyueCao <junyuecao@gmail.com>
2016-05-12 00:17:55 +0800

    Update sample

--- sample/src/main/java/me/zheteng/countrycodeselector/sample/MainActivity.java
+++ sample/src/main/java/me/zheteng/countrycodeselector/sample/MainActivity.java
@@ -2,14 +2,28 @@ package me.zheteng.countrycodeselector.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
+import me.zheteng.countrycodeselector.PhoneInputView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final PhoneInputView view = (PhoneInputView) findViewById(R.id.phone_input);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.dialog) {
+                    view.setCountrySelectorType(PhoneInputView.SELECTOR_TYPE_DIALOG);
+                } else if (checkedId == R.id.activity) {
+                    view.setCountrySelectorType(PhoneInputView.SELECTOR_TYPE_ACTIVITY);
+                }
+            }
+        });
     }
 }
